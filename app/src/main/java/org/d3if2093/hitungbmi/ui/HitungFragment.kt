e12329479732a028243af03a0522ba2d1d060e8a
ghifari <ghifarialqodri673@gmail.com>
2021-03-17 14:23:11 +0700

    Mengirim data saat bernavigasi

--- app/src/main/java/org/d3if2093/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if2093/hitungbmi/ui/HitungFragment.kt
@@ -9,15 +9,20 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import org.d3if2093.hitungbmi.R
+import org.d3if2093.hitungbmi.data.KategoriBmi
 import org.d3if2093.hitungbmi.databinding.FragmentHitungBinding
 
 class HitungFragment : Fragment () {
     private lateinit var binding: FragmentHitungBinding
+    private lateinit var kategoriBmi: KategoriBmi
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
         binding.button.setOnClickListener{ hitungBmi() }
-        binding.saranButton.setOnClickListener { view:View -> view.findNavController().navigate(
-                R.id.action_hitungFragment_to_saranFragment
+        binding.saranButton.setOnClickListener { view:View ->
+            view.findNavController().navigate(
+            HitungFragmentDirections.
+                actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
         return binding.root
@@ -50,18 +55,23 @@ class HitungFragment : Fragment () {
         binding.saranButton.visibility = View.VISIBLE
     }
     private fun getKategori(bmi: Float, isMale: Boolean): String {
-        val stringRes = if (isMale){
+        kategoriBmi = if (isMale){
             when {
-                bmi < 20.5 -> R.string.kurus
-                bmi >= 27.0 -> R.string.gemuk
-                else -> R.string.ideal
+                bmi < 20.5 -> KategoriBmi.KURUS
+                bmi >= 27.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
             }
         }else{
             when{
-                bmi < 18.5 -> R.string.kurus
-                bmi >= 25.0 -> R.string.gemuk
-                else -> R.string.ideal
+                bmi < 18.5 -> KategoriBmi.KURUS
+                bmi >= 25.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
             }
+        }
+        val stringRes = when(kategoriBmi){
+            KategoriBmi.KURUS ->R.string.kurus
+            KategoriBmi.IDEAL ->R.string.ideal
+            KategoriBmi.GEMUK ->R.string.gemuk
         }
         return getString(stringRes)
     }
--- app/src/main/java/org/d3if2093/hitungbmi/ui/SaranFragment.kt
+++ app/src/main/java/org/d3if2093/hitungbmi/ui/SaranFragment.kt
@@ -6,17 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import org.d3if2093.hitungbmi.R
 import org.d3if2093.hitungbmi.data.KategoriBmi
 import org.d3if2093.hitungbmi.databinding.FragmentSaranBinding
 
 class SaranFragment : Fragment () {
+
+    private val args: SaranFragmentArgs by navArgs()
     private lateinit var binding: FragmentSaranBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentSaranBinding.inflate(layoutInflater,container,false)
-        updateUI(KategoriBmi.KURUS)
+        updateUI(args.kategori)
         return  binding.root
     }
     private fun updateUI(kategori:KategoriBmi){
